8a9db7b9545b2797de7f3fe8ffc187b88cfea542
kdokic1 <kdokic1@etf.unsa.ba>
2021-03-26 11:24:53 +0100

    created get route for all reports for one user

---
+++ MonitorWebAPI/MonitorWebAPI/Models/ReportResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonitorWebAPI.Models
+{
+    public class ReportResponseModel
+    {
+        public int ReportId { get; set; }
+        public string Name { get; set; }
+        public string Query { get; set; }
+        public string Frequency { get; set; }
+        public DateTime StartDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
