108acad8b3a8d3f10be627e921f9e41a6d3115a5
msehalic1 <msehalic1@etf.unsa.ba>
2021-04-01 19:39:16 +0200

    Added endpoints for report GET, POST and DELETEwith query parameters - phase 1

--- MonitorWebAPI/MonitorWebAPI/Controllers/ReportController.cs
+++ MonitorWebAPI/MonitorWebAPI/Controllers/ReportController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MonitorWebAPI.Helpers;
 using MonitorWebAPI.Models;
@@ -8,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MonitorWebAPI.Controllers
@@ -38,7 +38,7 @@ namespace MonitorWebAPI.Controllers
                 string responseBody = await response.Content.ReadAsStringAsync();
                 VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
 
-                List<Report> allReports = mc.Reports.Where(x => x.UserId == vu.id).ToList();
+                List<Report> allReports = mc.Reports.Where((x => x.UserId == vu.id)).Where(y => y.Deleted == false).ToList();
 
                 List<ReportResponseModel> reportList = new List<ReportResponseModel>();
 
@@ -50,7 +50,7 @@ namespace MonitorWebAPI.Controllers
                         Name = report.Name,
                         Query = report.Query,
                         Frequency = report.Frequency,
-                        StartDate = report.StartDate,
+                        StartDate = report.NextDate,
                         UserId = vu.id
                     });
                 }
@@ -63,5 +63,209 @@ namespace MonitorWebAPI.Controllers
                 return Unauthorized();
             }
         }
+
+        [Route("api/report/GetReports")]
+        [HttpGet]
+        public async Task<ActionResult<ResponseModel<List<ReportResponseModel>>>> GetReports([FromHeader] string Authorization, [FromQuery] ReportResponseModel queryReport)
+        {
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null)
+            {
+                return Unauthorized();
+            }
+
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+
+                List<Report> allReports = mc.Reports.ToList().Where(x => x.Deleted == false).ToList();
+
+                if (queryReport.Frequency != null)
+                {
+                    allReports = allReports.Where(x => x.Frequency == queryReport.Frequency).ToList();
+                }
+                if (string.IsNullOrEmpty(queryReport.ReportId.ToString()))
+                {
+                    allReports = allReports.Where(x => x.ReportId == queryReport.ReportId).ToList();
+                }
+                if (queryReport.Name != null)
+                {
+                    allReports = allReports.Where(x => x.Name == queryReport.Name).ToList();
+                }
+                if (!string.IsNullOrEmpty(queryReport.UserId.ToString()))
+                {
+                    allReports = allReports.Where(x => x.UserId == queryReport.UserId).ToList();
+                }
+                if (queryReport.StartDate != DateTime.MinValue)
+                {
+                    allReports = allReports.Where(x => x.NextDate.Ticks > queryReport.StartDate.Ticks).ToList();
+                }
+                if (!string.IsNullOrEmpty(queryReport.UserId.ToString()))
+                {
+                    allReports = allReports.Where(x => x.UserId == queryReport.UserId).ToList();
+                }
+
+                List<ReportResponseModel> reportList = new List<ReportResponseModel>();
+
+                foreach (var report in allReports)
+                {
+                    reportList.Add(new ReportResponseModel()
+                    {
+                        ReportId = report.ReportId,
+                        Name = report.Name,
+                        Query = report.Query,
+                        Frequency = report.Frequency,
+                        StartDate = report.NextDate,
+                        UserId = vu.id
+                    });
+                }
+
+                return new ResponseModel<List<ReportResponseModel>>() { data = reportList, newAccessToken = vu.accessToken };
+
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [Route("api/report/CreateReport")]
+        [HttpPost]
+        public async Task<ActionResult<ResponseModel<ReportResponseModel>>> CreateReport([FromHeader] string Authorization, [FromBody] Report report)
+        {
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null)
+            {
+                return Unauthorized();
+            }
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+                try
+                {
+                    report.User = mc.Users.Where(x => x.UserId == vu.id).FirstOrDefault();
+                    mc.Reports.Add(report);
+                        await mc.SaveChangesAsync();
+                        ReportResponseModel tempReport = new ReportResponseModel()
+                        {
+                            ReportId = report.ReportId,
+                            Name = report.Name,
+                            Query = report.Query,
+                            Frequency = report.Frequency,
+                            StartDate = report.NextDate,
+                            UserId = vu.id
+                        };
+
+                        if (tempReport != null)
+                        {
+                            await mc.SaveChangesAsync();
+                            return new ResponseModel<ReportResponseModel>() { data = tempReport, newAccessToken = vu.accessToken };
+                        }
+                        throw new Exception("Report wasn't added succesfully");
+
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message + "\n" + e.InnerException);
+                }
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [Route("api/report/StopReport")]
+        [HttpPut]
+        public async Task<ActionResult<ResponseModel<List<ReportResponseModel>>>> StopReport([FromHeader] string Authorization, [FromBody] Report reportToStop)
+        {
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null)
+            {
+                return Unauthorized();
+            }
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+
+                List<Report> allReports = mc.Reports.Where(x => x.Deleted == false).ToList();
+
+                var existingReport = allReports.Where(x => x.ReportId == reportToStop.ReportId).FirstOrDefault();
+
+                if (existingReport != null)
+                {
+                    existingReport.Deleted = true;
+                    await mc.SaveChangesAsync();
+                }
+                List<ReportResponseModel> reportList = new List<ReportResponseModel>();
+
+                foreach (var report in allReports)
+                {
+                    reportList.Add(new ReportResponseModel()
+                    {
+                        ReportId = report.ReportId,
+                        Name = report.Name,
+                        Query = report.Query,
+                        Frequency = report.Frequency,
+                        StartDate = report.NextDate,
+                        UserId = vu.id,
+                    });
+                }
+
+                return new ResponseModel<List<ReportResponseModel>>() { data = reportList, newAccessToken = vu.accessToken };
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        //[Route("api/report/GetReportsByInstanceId/{instanceID}")]
+        //[HttpGet]
+        //public async Task<ActionResult<ResponseModel<List<ReportResponseModel>>>> GetReportsByInstanceID([FromHeader] string Authorization, [FromQuery] int instanceID)
+        //{
+        //    string JWT = JWTVerify.GetToken(Authorization);
+        //    if (JWT == null)
+        //    {
+        //        return Unauthorized();
+        //    }
+
+        //    HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string responseBody = await response.Content.ReadAsStringAsync();
+        //        VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+
+        //        List<Report> allReports = mc.Reports.ToList();
+
+        //        List<ReportResponseModel> reportList = new List<ReportResponseModel>();
+
+        //        foreach (var report in allReports)
+        //        {
+        //            reportList.Add(new ReportResponseModel()
+        //            {
+        //                ReportId = report.ReportId,
+        //                Name = report.Name,
+        //                Query = report.Query,
+        //                Frequency = report.Frequency,
+        //                StartDate = report.NextDate,
+        //                UserId = vu.id
+        //            });
+        //        }
+
+        //        return new ResponseModel<List<ReportResponseModel>>() { data = reportList, newAccessToken = vu.accessToken };
+
+        //    }
+        //    else
+        //    {
+        //        return Unauthorized();
+        //    }
+        //}
     }
 }
