c791138aa299e844e26f8826d7f562df70a50ea3
Huso Hamzić <56176904+hhamzic1@users.noreply.github.com>
2021-04-08 17:18:55 +0200

    Upload images edited, get images for user task implemented

--- MonitorWebAPI/MonitorWebAPI/Controllers/FileUploadController.cs
+++ MonitorWebAPI/MonitorWebAPI/Controllers/FileUploadController.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using MonitorWebAPI.Helpers;
 using MonitorWebAPI.Models;
-
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace MonitorWebAPI.Controllers
 {
@@ -17,18 +18,19 @@ namespace MonitorWebAPI.Controllers
     {
         private readonly IWebHostEnvironment hostingEnvironment;
         private readonly monitorContext mc;
+        private readonly HelperMethods hm;
 
         public FIleUploadController(IWebHostEnvironment he)
         {
             hostingEnvironment = he;
             mc = new monitorContext();
-
+            hm = new HelperMethods();
         }
 
 
         [Route("api/upload/UploadFile")]
         [HttpPost]
-        public ActionResult UploadFileFromFormData([FromForm] IFormCollection form, [FromHeader] string Authorization)
+        public async Task<ActionResult> UploadFileFromFormData([FromForm] IFormCollection form, [FromHeader] string Authorization, Guid deviceUid, int taskId)
         {
             string JWT = JWTVerify.GetToken(Authorization);
             if (JWT == null)
@@ -38,31 +40,44 @@ namespace MonitorWebAPI.Controllers
             HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
             if (response.IsSuccessStatusCode)
             {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+                var userRoleName = mc.Roles.Where(x => x.RoleId == vu.roleId).FirstOrDefault().Name;
                 try
                 {
-                    var files = form.Files;
-                    var folderName = Path.Combine("Uploads", "Files");
-                    var pathToSave = Path.Combine(hostingEnvironment.ContentRootPath, folderName);
-                    if (!Directory.Exists(pathToSave))
-                    {
-                        Directory.CreateDirectory(pathToSave);
-                    }
-                    if (files.Any(f => f.Length == 0 || String.IsNullOrEmpty(f.Name)))
-                    {
-                        return BadRequest();
-                    }
+                    int deviceId = mc.Devices.Where(x => x.DeviceUid == deviceUid).FirstOrDefault().DeviceId;
+                    var tempTask = mc.UserTasks.Where(x => x.UserId == vu.id && x.TaskId == taskId).FirstOrDefault();
 
-                    foreach (var file in files)
+                    if (userRoleName == "MonitorSuperAdmin" || (userRoleName == "SuperAdmin" && tempTask != null && hm.CheckIfDeviceBelongsToUsersTree(vu, deviceId)))
                     {
-                        var fileName = file.Name + Path.GetExtension(file.FileName);
-                        var fullPath = Path.Combine(pathToSave, fileName);
-                        var dbPath = Path.Combine(folderName, fileName);
-                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        var files = form.Files;
+                        var folderName = Path.Combine("Uploads", "Files");
+                        var pathToSave = Path.Combine(hostingEnvironment.ContentRootPath, folderName);
+                        if (!Directory.Exists(pathToSave))
                         {
-                            file.CopyTo(stream);
+                            Directory.CreateDirectory(pathToSave);
                         }
+                        if (files.Any(f => f.Length == 0 || String.IsNullOrEmpty(f.Name)))
+                        {
+                            return BadRequest();
+                        }
+
+                        foreach (var file in files)
+                        {
+                            var fileName = file.Name + Path.GetExtension(file.FileName);
+                            var fullPath = Path.Combine(pathToSave, fileName);
+                            var dbPath = Path.Combine(folderName, fileName);
+                            using (var stream = new FileStream(fullPath, FileMode.Create))
+                            {
+                                file.CopyTo(stream);
+                            }
+                        }
+                        return Ok("All files are successfully uploaded.");
                     }
-                    return Ok("All the files are successfully uploaded.");
+                    else
+                    {
+                        return StatusCode(403);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -74,27 +89,59 @@ namespace MonitorWebAPI.Controllers
                 return Unauthorized();
             }
         }
-        /*
-        [Route("api/upload/UploadFileByteArray")]
-        [HttpPost]
-        public ActionResult UploadFile([FromBody]byte[] f, string fileName)
+
+        [HttpGet]
+        [Route("api/upload/GetImagesForUserTask")]
+        public async Task<ActionResult<ResponseModel<List<ImageModel>>>> GetImagesForUserTask([FromHeader] string Authorization, Guid deviceUid, int taskId)
         {
-            try
+            List<ImageModel> images = new List<ImageModel>();
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null)
             {
-                var folderName = Path.Combine("Uploads", "Files");
-                var pathToSave = Path.Combine(hostingEnvironment.ContentRootPath, folderName);
-                MemoryStream ms = new MemoryStream(f);
-                FileStream fs = new FileStream(pathToSave, FileMode.Create);
-                ms.WriteTo(fs);
-                ms.Close();
-                fs.Close();
-                fs.Dispose();
-                return Ok("All good");
+                return Unauthorized();
             }
-            catch (Exception)
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (response.IsSuccessStatusCode)
             {
-                return BadRequest("Not good at all");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+                var userRoleName = mc.Roles.Where(x => x.RoleId == vu.roleId).FirstOrDefault().Name;
+                try
+                {
+
+                    int deviceId = mc.Devices.Where(x => x.DeviceUid == deviceUid).FirstOrDefault().DeviceId;
+                    var tempTask = mc.UserTasks.Where(x => x.UserId == vu.id && x.TaskId == taskId).FirstOrDefault();
+                    if (userRoleName == "MonitorSuperAdmin" || (userRoleName == "SuperAdmin" && tempTask != null && hm.CheckIfDeviceBelongsToUsersTree(vu, deviceId)))
+                    {
+
+                        var folderName = Path.Combine("Uploads", "Files");
+                        var loadPath = Path.Combine(hostingEnvironment.ContentRootPath, folderName);
+                        string matching = deviceUid + "_" + taskId + '*';
+                        foreach (string file in Directory.GetFiles(loadPath, matching))
+                        {
+                            byte[] bytes = System.IO.File.ReadAllBytes(file);
+                            images.Add(new ImageModel
+                            {
+                                Name = Path.GetFileName(file),
+                                Base64 = Convert.ToBase64String(bytes, 0, bytes.Length)
+                            });
+                        }
+                        return new ResponseModel<List<ImageModel>> { data = images, newAccessToken = vu.accessToken };
+                    }
+                    else
+                    {
+                        return StatusCode(403);
+                    }
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
             }
-        }*/
+            else
+            {
+                return Unauthorized();
+            }
+        }
     }
 }
---
+++ MonitorWebAPI/MonitorWebAPI/Models/ImageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorWebAPI.Models
+{
+    public class ImageModel
+    {
+        public string Name { get; set; }
+        public string Base64 { get; set; }
+    }
+}
