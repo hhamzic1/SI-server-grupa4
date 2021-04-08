8d28b5cfa7c514b466ea91253b859d0ed554c1f7
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2021-04-08 18:00:52 +0200

    Implemented endpoints for getting command logs with specific user/device id

--- MonitorWebAPI/MonitorWebAPI/Controllers/UserCommandLogsController.cs
+++ MonitorWebAPI/MonitorWebAPI/Controllers/UserCommandLogsController.cs
@@ -59,5 +59,90 @@ namespace MonitorWebAPI.Controllers
                 return Unauthorized();
             }
         }
+
+        [HttpGet]
+        [Route("/api/user-command-logs/CommandLogsForDevice")]
+        public async Task<ActionResult<ResponseModel<List<UserCommandsLog>>>> GetAllCommandLogsForDevice([FromHeader] string Authorization, [FromQuery] int deviceId) {
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null) {
+                return Unauthorized();
+            }
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (!response.IsSuccessStatusCode) {
+                return Unauthorized();
+            }
+            string responseBody = await response.Content.ReadAsStringAsync();
+            VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+
+            if(checkIfDeviceWithIdExists(deviceId)) {
+                List<UserCommandsLog> listLogs = mc.UserCommandsLogs.Where(x => x.DeviceId == deviceId).ToList();
+
+                Device tempDev = mc.Devices.Where(x => x.DeviceId == deviceId).FirstOrDefault();
+                foreach (UserCommandsLog item in listLogs) {
+                    User tempUser = mc.Users.Where(x => x.UserId == item.UserId).FirstOrDefault();
+                    item.User = tempUser;
+                    item.Device = tempDev;
+                }
+
+                return new ResponseModel<List<UserCommandsLog>>() {
+                    data = listLogs,
+                    newAccessToken = vu.accessToken
+                };
+            }
+            return BadRequest("Device with said id does not exist");
+        }
+
+        [HttpGet]
+        [Route("api/user-command-logs/CommandLogsForDeviceAndUser")]
+        public async Task<ActionResult<ResponseModel<List<UserCommandsLog>>>> GetAllCommandLogsForDeviceAndUser([FromHeader] string Authorization, [FromQuery] int deviceId, [FromQuery] int userId) {
+            string JWT = JWTVerify.GetToken(Authorization);
+            if (JWT == null) {
+                return Unauthorized();
+            }
+            HttpResponseMessage response = JWTVerify.VerifyJWT(JWT).Result;
+            if (!response.IsSuccessStatusCode) {
+                return Unauthorized();
+            }
+            string responseBody = await response.Content.ReadAsStringAsync();
+            VerifyUserModel vu = JsonConvert.DeserializeObject<VerifyUserModel>(responseBody);
+
+            string message = "";
+            if (checkIfDeviceWithIdExists(deviceId)) {
+
+                if (checkIfUserWithIdExists(userId)) {
+                        List<UserCommandsLog> listLogs = mc.UserCommandsLogs.Where(x => x.DeviceId == deviceId)
+                                                        .Where(x => x.UserId == userId)
+                                                        .ToList();
+
+                        Device tempDev = mc.Devices.Where(x => x.DeviceId == deviceId).FirstOrDefault();
+                        User tempUser = mc.Users.Where(x => x.UserId == userId).FirstOrDefault();
+                        foreach (UserCommandsLog item in listLogs) {
+                            item.User = tempUser;
+                            item.Device = tempDev;
+                        }
+
+                        return new ResponseModel<List<UserCommandsLog>>() {
+                            data = listLogs,
+                            newAccessToken = vu.accessToken
+                        };
+
+                } else {
+                    message = "User with said id does not exist";
+                }
+
+            } else {
+                message = "Device with said id does not exist";
+            }
+            return BadRequest(message);
+        }
+
+
+        private bool checkIfDeviceWithIdExists(int id) {
+            return mc.Devices.Where(x => x.DeviceId == id).FirstOrDefault() != null;
+        }
+
+        private bool checkIfUserWithIdExists(int id) {
+            return mc.Users.Where(x => x.UserId == id).FirstOrDefault() != null;
+        }
     }
 }
