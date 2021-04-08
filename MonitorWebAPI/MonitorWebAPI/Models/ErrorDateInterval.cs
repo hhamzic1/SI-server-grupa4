6487d4caa8c8c058ecf9a737f8131deedf0f792e
Allen Kovacevic <akovacevic4@etf.unsa.ba>
2021-04-08 23:17:08 +0200

    Endpoints for getting errors

---
+++ MonitorWebAPI/MonitorWebAPI/Models/ErrorDateInterval.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorWebAPI.Models
+{
+    public class ErrorDateInterval
+    {
+        public Guid DeviceUID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ MonitorWebAPI/MonitorWebAPI/Models/ErrorInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorWebAPI.Models
+{
+    public class ErrorInfo
+    {
+        public DateTime ErrorTime { get; set; }
+        public string Message { get; set; }
+        public int? Code { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public Guid DeviceUID { get; set; }
+    }
+}
