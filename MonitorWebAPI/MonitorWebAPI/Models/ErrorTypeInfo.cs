9b56dcfc2ccb1a995f029d5179188bb385b7662c
PasaDzumhur <pdzumhur1@etf.unsa.ba>
2021-04-10 00:32:43 +0200

    refactor error get methods

---
+++ MonitorWebAPI/MonitorWebAPI/Models/ErrorTypeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorWebAPI.Models
+{
+    public class ErrorTypeInfo
+    {
+
+        public int? Code { get; set; }
+
+        public int? ErrorCodeNumber { get; set; }
+
+
+    }
+}
