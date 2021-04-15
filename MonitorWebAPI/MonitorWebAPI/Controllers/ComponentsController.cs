05b7ea6a231d71449d7c49f1c04c11bb009b7d9d
mmuhovic2 <mmuhovic2@etf.unsa.ba>
2021-04-15 18:36:37 +0200

    Grupa12 - mala izmjena u kodu

--- MonitorWebAPI/MonitorWebAPI/Controllers/ComponentsController.cs
+++ MonitorWebAPI/MonitorWebAPI/Controllers/ComponentsController.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MonitorWebAPI.Helpers;
 using MonitorWebAPI.Models;
 using Newtonsoft.Json;
-using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
+
 
 namespace MonitorWebAPI.Controllers
 {
