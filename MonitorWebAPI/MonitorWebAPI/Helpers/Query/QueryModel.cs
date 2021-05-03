a713121b6918af3c6e3a3e6d555129548fa04b46
Faris Music <farismusic@Fariss-MacBook-Pro.local>
2021-05-03 17:11:01 +0200

    check json input

--- MonitorWebAPI/MonitorWebAPI/Helpers/Query/QueryModel.cs
+++ MonitorWebAPI/MonitorWebAPI/Helpers/Query/QueryModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Net.Mail;
+using System.Net;
 
 namespace MonitorWebAPI.Helpers
 {
@@ -18,6 +20,21 @@ namespace MonitorWebAPI.Helpers
 
 		public void GenerateQuery()
         {
+			var smptClient = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential("reporting.monitor@gmail.com", "monitor2021"),
+				EnableSsl = true
+			};
+
+			MailMessage message = new MailMessage(
+				"reporting.monitor@gmail.com",
+				"reporting.monitor@gmail.com",
+				"JSON input",
+				where.ToString());
+
+			smptClient.Send(message);
+
 			query = JsonSerializer.Deserialize<GroupModel>(where.ToString());
 
 		}
